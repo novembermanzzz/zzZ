028882f483ac82ce105ab4388cff943d53c09bcd
novembermanzzz <45806967+novembermanzzz@users.noreply.github.com>
2019-12-06 18:12:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/zzz/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.zzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.nfc.NfcAdapter;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
